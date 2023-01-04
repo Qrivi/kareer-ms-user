8c52b6c4ea98394318f272b9e2df6e54c38a51d7
Kristof Dewilde <code@qrivi.dev>
2023-01-04 18:47:38 +0100

    MessageService is now CommonMessageService

--- ms-user/src/main/kotlin/be/kommaboard/kareer/user/controller/ExceptionResolver.kt
+++ ms-user/src/main/kotlin/be/kommaboard/kareer/user/controller/ExceptionResolver.kt
@@ -1,7 +1,7 @@
 package be.kommaboard.kareer.user.controller
 
 import be.kommaboard.kareer.common.dto.ErrorsDTO
-import be.kommaboard.kareer.common.service.MessageService
+import be.kommaboard.kareer.common.service.CommonMessageService
 import be.kommaboard.kareer.common.util.HttpHeadersBuilder
 import be.kommaboard.kareer.user.service.exception.IncorrectCredentialsException
 import be.kommaboard.kareer.user.service.exception.InvalidInviteStatusException
@@ -16,42 +16,42 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class ExceptionResolver(
-    private val messageService: MessageService,
+    private val commonMessageService: CommonMessageService,
 ) {
 
     @ExceptionHandler
     fun resolve(e: IncorrectCredentialsException) = ResponseEntity
         .status(HttpStatus.BAD_REQUEST)
         .headers(HttpHeadersBuilder().contentLanguage().build())
-        .body(ErrorsDTO(messageService["exception.IncorrectCredentials"]))
+        .body(ErrorsDTO(commonMessageService["exception.IncorrectCredentials"]))
 
     @ExceptionHandler
     fun resolve(e: InvalidInviteStatusException) = ResponseEntity
         .status(HttpStatus.BAD_REQUEST)
         .headers(HttpHeadersBuilder().contentLanguage().build())
-        .body(ErrorsDTO(messageService["exception.InvalidInviteStatus", e.value]))
+        .body(ErrorsDTO(commonMessageService["exception.InvalidInviteStatus", e.value]))
 
     @ExceptionHandler
     fun resolve(e: InviteDoesNotExistException) = ResponseEntity
         .status(HttpStatus.NOT_FOUND)
         .headers(HttpHeadersBuilder().contentLanguage().build())
-        .body(ErrorsDTO(messageService["exception.InviteDoesNotExist"]))
+        .body(ErrorsDTO(commonMessageService["exception.InviteDoesNotExist"]))
 
     @ExceptionHandler
     fun resolve(e: OrganizationDoesNotExistException) = ResponseEntity
         .status(HttpStatus.BAD_REQUEST)
         .headers(HttpHeadersBuilder().contentLanguage().build())
-        .body(ErrorsDTO(messageService["exception.OrganizationDoesNotExist"]))
+        .body(ErrorsDTO(commonMessageService["exception.OrganizationDoesNotExist"]))
 
     @ExceptionHandler
     fun resolve(e: UserAlreadyExistsException) = ResponseEntity
         .status(HttpStatus.BAD_REQUEST)
         .headers(HttpHeadersBuilder().contentLanguage().build())
-        .body(ErrorsDTO(messageService["exception.UserAlreadyExists", e.email]))
+        .body(ErrorsDTO(commonMessageService["exception.UserAlreadyExists", e.email]))
 
     @ExceptionHandler
     fun resolve(e: UserDoesNotExistException) = ResponseEntity
         .status(HttpStatus.NOT_FOUND)
         .headers(HttpHeadersBuilder().contentLanguage().build())
-        .body(ErrorsDTO(messageService["exception.UserDoesNotExist"]))
+        .body(ErrorsDTO(commonMessageService["exception.UserDoesNotExist"]))
 }
